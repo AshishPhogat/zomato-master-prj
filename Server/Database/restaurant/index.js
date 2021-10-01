b2d90ad4861f915f623ebac3009d0025437509c9
AshishPhogat <ashishphog@gmail.com>
2021-10-01 19:58:17 +0530

    started the project

---
+++ Server/Database/restaurant/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const RestaruantSchema = new mongoose.Schema({
+    name : {type : string ,required : true },
+    city  : {type : string ,required : true },
+    address : {type : string ,required : true },
+    mapLocation  : {type : string ,required : true },
+    cuisine : [String],
+    restaurantTimings : String,
+    contactNumbe : Number ,
+    websites : Number ,
+    popularDishes : [String],
+    averageCost : Number,
+    amenities : [String],
+    menuImages :{
+        type : mongoose.Types.ObjectId,
+        ref : "Images",
+    },
+    menu :  {
+        type : mongoose.Types.ObjectId,
+        ref : "Menus",
+    },
+    reviews :[{type : mongoose.Types.ObjectId,ref : "Reviews"}],
+    photos :{type :mongoose.Types.ObjectId,ref :"Images"},
+});
+
+export const RestaurantModel = mongoose.Model("Restaurants",RestaruantSchema);
---
+++ Server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+
+const zomato = express();
+
+//application middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended : false}));
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/",(req,res)=>{
+    res.json({"message" : "setup successful"});
+});
+
+zomato.listen(4000,()=>{console.log("Server is running successfully!!!🚀")});
