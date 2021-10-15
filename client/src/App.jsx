13d0f95689c95658ef0aa1c335ce5afcbc4f5ea3
AshishPhogat <ashishphog@gmail.com>
2021-10-16 03:19:06 +0530

    checkout page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,55 @@
-import React from "react";
-import {Route,Redirect} from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
-//HOC 
-import HomeLayoutHOC from "./HOC/home.hoc";
+// HOC
+import HomeLayoutHOC from "./HOC/Home.Hoc";
 import RestaurantLayoutHOC from "./HOC/restaurant.hoc";
+import CheckoutLayoutHOC from "./HOC/Checkout.Hoc";
 
-
-//components
-import Temp from "./Components/temp";
-
-//pages
+// pages
 import Home from "./Page/home";
-import OverviewPage from "./Page/restaurant/overviewPage";
+import Overview from "./Page/restaurant/overviewPage";
 import OrderOnline from "./Page/restaurant/order-online";
 import Reviews from "./Page/restaurant/Reviews";
 import Menu from "./Page/restaurant/Menu";
 import Photos from "./Page/restaurant/Photos";
+import Checkout from "./Page/Checkout";
 
 function App() {
-  return<>
-      <Route path="/" exact >
-          <Redirect to="/delivery" />
+  return (
+    <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
       </Route>
-      <Route path="/restaurant/:id" exact >
-          <Redirect to="/restaurant/:id/overview" />
+      <Route path="/restaurant/:id" exact>
+        <Redirect to="/restaurant/:id/overview" />
       </Route>
-      <HomeLayoutHOC path="/:type" exact  component= {Home}  />
-      
-      <RestaurantLayoutHOC path="/restaurant/:id/overview" exact component={OverviewPage} />
-      <RestaurantLayoutHOC path="/restaurant/:id/order-online" exact component={OrderOnline} />
+
+      <HomeLayoutHOC path="/:type" exact component={Home} />
+
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/overview"
+        exact
+        component={Overview}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/order-online"
+        exact
+        component={OrderOnline}
+      />
       <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Menu} />
-      <RestaurantLayoutHOC path="/restaurant/:id/reviews" exact component={Reviews} />
-      <RestaurantLayoutHOC path="/restaurant/:id/photos" exact component={Photos} />
-  </>
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/reviews"
+        exact
+        component={Reviews}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/photos"
+        exact
+        component={Photos}
+      />
+      <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
+    </>
+  );
 }
 
 export default App;
